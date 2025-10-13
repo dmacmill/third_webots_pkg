69ba5e92749f1c09b9b91d60030d13dffe09650b
dmacmi.dm@gmail.com <dmacmi.dm@gmail.com>
2025-10-12 20:05:37 -0400

    added capabilities from turtlebot
    - controller_manager for event loop controllers
    - diffdrive_controller computes motion and pubs odom/tf
    - joint_state for updating /joint_states (not tf!)
    - robot_model.urdf for rviz2 to use (not necessary)

--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -1,28 +1,96 @@
 import os
 import launch
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
+from webots_ros2_driver.wait_for_controller_connection import WaitForControllerConnection
 
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
     package_dir = get_package_share_directory('third_webots_pkg')
     robot_description_path = os.path.join(package_dir, 'resource', 'third_webots_robot.urdf')
+    ros2_control_params = os.path.join(package_dir, 'resource', 'ros2control.yml')
+    use_twist_stamped = 'ROS_DISTRO' in os.environ and (os.environ['ROS_DISTRO'] in ['rolling', 'jazzy', 'kilted'])
+    if use_twist_stamped:
+        mappings = [('/diffdrive_controller/cmd_vel', '/cmd_vel'), ('/diffdrive_controller/odom', '/odom')]
+    else:
+        mappings = [('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel'), ('/diffdrive_controller/odom', '/odom')]
 
     webots = WebotsLauncher(
-        world=os.path.join(package_dir, 'worlds', 'my_world.wbt')
+        world=os.path.join(package_dir, 'worlds', 'my_world.wbt'),
+        ros2_supervisor=True
     )
+
+    footprint_publisher = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments= ['0', '0', '0', '0', '0', '0', 'base_link', 'base_footprint']
+    )
+
+    # robot_state_publisher = Node(
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     output="screen",
+    #     parameters=[{
+    #         'robot_description': '<robot name=""><link name=""/></robot>'
+    #     }]
+    # )
+
+    # ROS Control Spawners
+    controller_manager_timeout = ['--controller-manager-timeout', '50']
+    controller_manager_prefix = 'python.exe' if os.name == 'nt' else 'python3'
+    diffdrive_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        output="screen",
+        prefix=controller_manager_prefix,
+        arguments=['diffdrive_controller'] + controller_manager_timeout
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable='spawner',
+        output="screen",
+        prefix=controller_manager_prefix,
+        arguments=['joint_state_broadcaster'] + controller_manager_timeout,
+    )
+
+    ros_control_spawners = [diffdrive_controller_spawner, joint_state_broadcaster_spawner]
+    # ros_control_spawners = [diffdrive_controller_spawner]
 
     my_robot_driver = WebotsController(
         robot_name='my_robot',
         parameters=[
-            {'robot_description': robot_description_path},
-        ]
+            {'robot_description': robot_description_path,
+             'use_sim_time': use_sim_time,
+             'set_robot_state_publisher': True},
+             ros2_control_params
+        ],
+        remappings=mappings,
+        respawn=True
+    )
+
+
+    waiting_nodes = WaitForControllerConnection(
+        target_driver=my_robot_driver,
+        nodes_to_start=ros_control_spawners
     )
 
     return LaunchDescription([
         webots,
+        webots._supervisor,
+
+        # robot_state_publisher,
+        footprint_publisher,
+
+        # my_robot_driver,
+        waiting_nodes,
+
         my_robot_driver,
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
--- setup.py
+++ setup.py
@@ -10,8 +10,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name + '/launch', ['launch/robot_launch.py']),              # remember these
-	('share/' + package_name + '/worlds', ['worlds/my_world.wbt']),                 # in future 
-	('share/' + package_name + '/resource', ['resource/third_webots_robot.urdf']),  # setup.py files!
+	    ('share/' + package_name + '/worlds', ['worlds/my_world.wbt']),                 # in future 
+	    ('share/' + package_name + '/resource', [
+            'resource/third_webots_robot.urdf',
+            'resource/ros2control.yml'
+        ]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
